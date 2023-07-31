880a5c5257387fccd806ff5b4aac5841d5ba3e85
david <david_milagre@hotmail.com>
2023-07-30 23:47:07 -0300

    Initial commit

---
+++ src/main/java/milagre/cadastro/Cadastro.java
@@ -0,0 +1,24 @@
+package milagre.cadastro;
+
+
+public class Cadastro {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String senha;
+
+    //
+    public Cadastro(int id, String nome, String cpf, String email, String telefone, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+
+
+}
